e75b8bf989cc8747b4aaf375605b74a942181ab9
Piotr Tomczyk <piotr.tomczyk14@gmail.com>
2022-11-09 02:04:14 +0100

    Display descending ordered TotalConfirmed values

--- src/components/CountriesList.vue
+++ src/components/CountriesList.vue
@@ -1,10 +1,41 @@
 <template>
     <div>
         <h1>Countries List</h1>
+        <div v-for="country in sortedArray" :key="country.id">
+            <p>{{country.TotalConfirmed}}</p>
+        </div>
     </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
-    name:"CountriesList"
+    name:"CountriesList",
+    data() {
+        return {
+            content: []
+        }
+    },
+    computed: {
+        sortedArray: function() {
+            function compare(a, b) {
+            if (a.TotalConfirmed > b.TotalConfirmed)
+                return -1;
+            if (a.TotalConfirmed < b.TotalConfirmed)
+                return 1;
+            return 0;
+            }
+            return this.content.slice().sort(compare);
+            
+        }
+    },
+    mounted()
+    {
+        axios.get('https://api.covid19api.com/summary')
+        .then((response) => {
+            this.content = response.data.Countries
+            console.log(this.content.Countries[1].Country)
+            this.content.order
+        })
+    },
 }
 </script>
